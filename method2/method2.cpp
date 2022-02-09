d92484fe84e00294e4e70fb25393f435b390928a
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-02-09 22:44:19 +0800

    update

--- method2/method2.cpp
+++ method2/method2.cpp
@@ -1,4 +1,11 @@
-﻿//https://www.cnblogs.com/caishunzhe/p/12833859.html
+﻿/*
+
+这个项目的目的是利用ntfs 的 mft 来遍历C盘的文件
+
+*/
+
+//https://www.cnblogs.com/caishunzhe/p/12833859.html
+//https://zh.wikipedia.org/wiki/NTFS
 
 #include <iostream>
 #include<windows.h>
@@ -12,14 +19,68 @@ using namespace std;
 //x86有调用约定方面的各种各样的问题
 #pragma comment(lib,"ntdll")
 
-
-WCHAR Volume[] = L"\\\\.\\C:";
+#define VOLUME_PATH  L"\\\\.\\C:"
+#define FULL_PATH    L"\\??\\C:\\symbols"
+WCHAR Volume[] = VOLUME_PATH;
 char mybuffer[0x8000];
+
+typedef struct _EXTENT {
+
+    LONGLONG Vcn;
+    LONGLONG Lcn;
+    LONGLONG Length;
+
+} EXTENT, * PEXTENT;
+
+#define MAX_EXTENTS 64
 
 LARGE_INTEGER MftStart;
 ULONG ClusterSize;
 ULONG FrsSize;
+EXTENT Extents[MAX_EXTENTS];
+PCWCHAR FileName = L"pingme.txt";
+LONGLONG CachedOffset = -1;
+UCHAR CacheBuffer[0x10000];   // max cluster size
 
+
+LONGLONG
+ComputeFileRecordLbo(
+    IN ULONG MftIndex
+)
+{
+    LONGLONG vcn;
+    LONGLONG lcn = 0;
+    ULONG extentIndex;
+    ULONG offsetWithinCluster;
+
+    vcn = (MftIndex * FrsSize) / ClusterSize;
+
+    for (extentIndex = 0; extentIndex < MAX_EXTENTS; extentIndex += 1) {
+
+        if ((vcn >= Extents[extentIndex].Vcn) &&
+            (vcn < Extents[extentIndex].Vcn + Extents[extentIndex].Length)) {
+
+            lcn = Extents[extentIndex].Lcn + (vcn - Extents[extentIndex].Vcn);
+        }
+    }
+
+    if (ClusterSize >= FrsSize) {
+
+        offsetWithinCluster = (MftIndex % (ClusterSize / FrsSize)) * FrsSize;
+        return (lcn * ClusterSize + offsetWithinCluster);
+
+    }
+    else {
+
+        //
+        //  BUGBUG keithka 4/28/00 Handle old fashioned big frs and/or big
+        //  clusters someday.
+        //
+
+        assert(FALSE);
+        return 0;
+    }
+}
 
 VOID
 FindAttributeInFileRecord(
@@ -103,6 +164,217 @@ FindAttributeInFileRecord(
     return;
 }
 
+BOOLEAN
+FindNameInFileRecord(
+    IN PFILE_RECORD_SEGMENT_HEADER FileRecord,
+    IN PCWCHAR FileName,
+    IN ULONG FileNameLength
+)
+{
+    PATTRIBUTE_RECORD_HEADER attr;
+    PFILE_NAME fileNameAttr;
+    ULONG cmpResult;
+
+    FindAttributeInFileRecord(FileRecord,
+        $FILE_NAME,
+        NULL,
+        &attr);
+
+    while (NULL != attr) {
+
+        if (((PUCHAR)attr - (PUCHAR)FileRecord) > (LONG)FrsSize) {
+
+            assert(FALSE);
+            return FALSE;
+        }
+
+        //
+        //  Names shouldn't go nonresident.
+        //
+
+        if (attr->FormCode != RESIDENT_FORM) {
+
+            assert(FALSE);
+            return FALSE;
+        }
+
+        fileNameAttr = (PFILE_NAME)((PUCHAR)attr + attr->Form.Resident.ValueOffset);
+
+        if (fileNameAttr->FileNameLength == FileNameLength) {
+
+            cmpResult = wcsncmp(FileName,
+                (PWCHAR)fileNameAttr->FileName,
+                fileNameAttr->FileNameLength);
+
+            if (0 == cmpResult) {
+
+                return TRUE;
+            }
+
+        }
+
+        printf("\nNot a match %S,%S", FileName, fileNameAttr->FileName);
+
+        //
+        //  Find the next filename, if any.
+        //
+
+        FindAttributeInFileRecord(FileRecord,
+            $FILE_NAME,
+            attr,
+            &attr);
+    }
+
+    return FALSE;
+}
+
+
+int
+FsTestOpenById(
+    IN UCHAR* ObjectId,
+    IN HANDLE VolumeHandle
+)
+{
+    HANDLE File;
+    IO_STATUS_BLOCK IoStatusBlock;
+    NTSTATUS Status;
+    NTSTATUS GetNameStatus;
+    NTSTATUS CloseStatus;
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    UNICODE_STRING str;
+    WCHAR nameBuffer[MAX_PATH];
+    PFILE_NAME_INFORMATION FileName;
+    WCHAR Full[] = FULL_PATH;        // Arrays of WCHAR's aren't constants
+
+    RtlInitUnicodeString(&str, Full);
+
+    str.Length = 8;
+    RtlCopyMemory(&str.Buffer[0],  //  no device prefix for relative open.
+        ObjectId,
+        8);
+
+    InitializeObjectAttributes(&ObjectAttributes,
+        &str,
+        OBJ_CASE_INSENSITIVE,
+        VolumeHandle,
+        NULL);
+
+    Status = NtCreateFile(&File,
+        GENERIC_READ,
+        &ObjectAttributes,
+        &IoStatusBlock,
+        NULL,
+        FILE_ATTRIBUTE_NORMAL,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        FILE_OPEN,
+        FILE_OPEN_BY_FILE_ID,
+        NULL,
+        0);
+
+    if (NT_SUCCESS(Status)) {
+
+        RtlZeroMemory(nameBuffer, sizeof(nameBuffer));
+        FileName = (PFILE_NAME_INFORMATION)&nameBuffer[0];
+        FileName->FileNameLength = sizeof(nameBuffer) - sizeof(ULONG);
+
+        GetNameStatus = NtQueryInformationFile(File,
+            &IoStatusBlock,
+            FileName,
+            sizeof(nameBuffer),
+            FileNameInformation);
+
+        printf("%S\n", FileName->FileName);
+
+        CloseStatus = NtClose(File);
+
+        if (!NT_SUCCESS(CloseStatus)) {
+
+            printf("\nCloseStatus %x", CloseStatus);
+        }
+    }
+
+    return Status;
+}
+
+NTSTATUS
+ReadFileRecord(
+    IN HANDLE VolumeHandle,
+    IN ULONG RecordIndex,
+    IN OUT PVOID Buffer
+)
+{
+    NTSTATUS status;
+    LARGE_INTEGER byteOffset;
+    IO_STATUS_BLOCK ioStatusBlock;
+    ULONG offsetWithinBuffer;
+
+    byteOffset.QuadPart = ComputeFileRecordLbo(RecordIndex);
+
+    if (FrsSize >= ClusterSize) { //mftentry的大小 > 簇大小
+
+        status = NtReadFile(VolumeHandle,
+            NULL,            //  Event
+            NULL,            //  ApcRoutine
+            NULL,            //  ApcContext
+            &ioStatusBlock,
+            Buffer,
+            FrsSize,
+            &byteOffset,    //  ByteOffset
+            NULL);         //  Key
+
+    }
+    else {
+
+        //
+        //  Clusters bigger than filerecords, do cluster
+        //  size reads and dice up the returns.
+        //
+
+        if ((-1 == CachedOffset) ||
+            (byteOffset.QuadPart < CachedOffset) ||
+            ((byteOffset.QuadPart + FrsSize) > (CachedOffset + ClusterSize))) {
+
+            printf("\nCache miss at %I64x", byteOffset.QuadPart);
+            
+
+            status = NtReadFile(VolumeHandle,
+                NULL,            //  Event
+                NULL,            //  ApcRoutine
+                NULL,            //  ApcContext
+                &ioStatusBlock,
+                CacheBuffer,
+                ClusterSize,
+                &byteOffset,    //  ByteOffset
+                NULL);         //  Key
+
+            if (0 != status) {
+
+                //
+                //  The cache buffer may be junk now, reread it next time.
+                //
+
+                CachedOffset = -1;
+                return status;
+            }
+
+            CachedOffset = byteOffset.QuadPart;
+            offsetWithinBuffer = 0;
+
+        }
+        else {
+
+
+            printf("\nCache hit at %I64x", byteOffset.QuadPart);
+
+            offsetWithinBuffer = (ULONG)(byteOffset.QuadPart % CachedOffset);
+            status = 0;
+        }
+
+        RtlCopyMemory(Buffer, CacheBuffer + offsetWithinBuffer, FrsSize);
+    }
+
+    return status;
+}
 
 int main()
 {
@@ -112,11 +384,24 @@ int main()
     NTSTATUS ReadStatus;
     PPACKED_BOOT_SECTOR bootSector;
     BIOS_PARAMETER_BLOCK bpb;
-    ULONG ClusterSize;
     IO_STATUS_BLOCK IoStatusBlock;
     LONGLONG mftBytesRead;
     PATTRIBUTE_RECORD_HEADER attr;
+    VCN nextVcn;
+    VCN currentVcn;
+    VCN vcnDelta;
+    LCN currentLcn;
+    LCN lcnDelta;
+    PUCHAR bsPtr;
+    UCHAR v;
+    UCHAR l;
+    UCHAR i;
+    ULONG extentCount;
+    ULONG recordIndex;
     ULONG mftRecords;
+    ULONG fileNameLength;
+    MFT_SEGMENT_REFERENCE segRef;
+    PFILE_RECORD_SEGMENT_HEADER DirMft;
 
     volumeHandle = CreateFileW(Volume,
         GENERIC_READ | GENERIC_WRITE,
@@ -173,7 +458,7 @@ int main()
     //
     ClusterSize = bpb.BytesPerSector * bpb.SectorsPerCluster;
 
-    //Mft大小一般是1KB
+    //Mft大小(FrsSize)一般是1KB
     if (bootSector->ClustersPerFileRecordSegment < 0) {
 
         FrsSize = 1 << (-1 * bootSector->ClustersPerFileRecordSegment);
@@ -187,6 +472,8 @@ int main()
     //集群数*集群大小=Mft起始位置
     //0xc0000000/512=0x600000(扇区起始地址数)
     MftStart.QuadPart = ClusterSize * bootSector->MftStartLcn;
+
+    //MftStart.QuadPart += 0xA * 512;  //dir mft的地址
 
     mftBytesRead = 0;
 
@@ -207,32 +494,129 @@ int main()
     }
 
     mftBytesRead += IoStatusBlock.Information;
-
+    
     FindAttributeInFileRecord((PFILE_RECORD_SEGMENT_HEADER)mybuffer,
         $DATA,
         NULL,
         &attr);
 
-    if (NULL == attr) {
 
-        printf("\nMFT record 0 has no $DATA attribute");
-        goto exit;
-    }
+    //DirMft = (PFILE_RECORD_SEGMENT_HEADER)mybuffer;
 
-    if (attr->FormCode == RESIDENT_FORM) {
-
-        printf("\nVolume has very few files, use dir /s");
-        goto exit;
-    }
-    
-    assert(attr->Form.Nonresident.FileSize <= ULONG_MAX);
     mftRecords = (ULONG)(attr->Form.Nonresident.FileSize / FrsSize);
 
+    nextVcn = attr->Form.Nonresident.LowestVcn;
+    currentLcn = 0;
+    extentCount = 0;
+    RtlZeroMemory(Extents, sizeof(Extents));
 
+    bsPtr = ((PUCHAR)attr) + attr->Form.Nonresident.MappingPairsOffset;
 
+    while (*bsPtr != 0) {
 
+        currentVcn = nextVcn;
 
+        //
+        //  Variable names v and l used for consistency with comments in 
+        //  ATTRIBUTE_RECORD_HEADER struct explaining how to decompress
+        //  mapping pair information.
+        //
 
+        v = (*bsPtr) & 0xf;
+        l = ((*bsPtr) & 0xf0) >> 4;
+
+        bsPtr += 1;
+
+        for (vcnDelta = 0, i = 0; i < v; i++) {
+
+            vcnDelta += *(bsPtr++) << (8 * i);
+        }
+
+        for (lcnDelta = 0, i = 0; i < l; i++) {
+
+            lcnDelta += *(bsPtr++) << (8 * i);
+        }
+
+        //
+        //  Sign extend.
+        //
+
+        if (0x80 & (*(bsPtr - 1))) {
+
+            for (; i < sizeof(lcnDelta); i++) {
+
+                lcnDelta += 0xff << (8 * i);
+            }
+        }
+
+        currentLcn += lcnDelta;
+        // printf( "\nVcn %I64x, Lcn %I64x, Length %I64x", currentVcn, currentLcn, vcnDelta );
+
+        if (extentCount < MAX_EXTENTS) {
+
+            Extents[extentCount].Vcn = currentVcn;
+            Extents[extentCount].Lcn = currentLcn;
+            Extents[extentCount].Length = vcnDelta;
+
+            extentCount += 1;
+
+        }
+        else {
+
+            printf("\nExcessive MFT fragmentation, redefine MAX_EXTENTS and recompile");
+        }
+
+        currentVcn += vcnDelta;
+    }
+
+    //
+    //  Now we know where the MFT is, let's go read it.
+    //
+
+    fileNameLength = wcslen(FileName);
+
+    for (recordIndex = 0; recordIndex <= mftRecords; recordIndex++) {
+
+        ReadStatus = ReadFileRecord(volumeHandle,
+            recordIndex,
+            mybuffer);
+
+        if (STATUS_SUCCESS != ReadStatus) {
+
+            printf("\nMFT record read failed with status %x", ReadStatus);
+            goto exit;
+        }
+
+        if (FindNameInFileRecord((PFILE_RECORD_SEGMENT_HEADER)mybuffer,
+            FileName,
+            fileNameLength)) {
+
+            //
+            //  Found a match, open by id and retrieve name.
+            //
+
+                printf("\nFound match in file %08x %08x\n",
+                    ((PFILE_RECORD_SEGMENT_HEADER)mybuffer)->SequenceNumber,
+                    recordIndex);
+
+            segRef.SegmentNumberLowPart = recordIndex;
+            segRef.SegmentNumberHighPart = 0;
+            segRef.SequenceNumber = ((PFILE_RECORD_SEGMENT_HEADER)mybuffer)->SequenceNumber;
+
+            FsTestOpenById((PUCHAR)&segRef, volumeHandle);
+        }
+
+        //
+        //  The number 0x400 is completely arbitrary.  It's a reasonable interval
+        //  of work to do before printing another period to tell the user we're 
+        //  making progress still.
+        //
+
+        if (0 == (recordIndex % 0x400)) {
+
+            printf(".");
+        }
+    }
 
 
 
--- method2/nt.h
+++ method2/nt.h
@@ -1,5 +1,149 @@
 #include<windows.h>
 
+#define FILE_DIRECTORY_FILE                     0x00000001
+#define FILE_WRITE_THROUGH                      0x00000002
+#define FILE_SEQUENTIAL_ONLY                    0x00000004
+#define FILE_NO_INTERMEDIATE_BUFFERING          0x00000008
+
+#define FILE_SYNCHRONOUS_IO_ALERT               0x00000010
+#define FILE_SYNCHRONOUS_IO_NONALERT            0x00000020
+#define FILE_NON_DIRECTORY_FILE                 0x00000040
+#define FILE_CREATE_TREE_CONNECTION             0x00000080
+
+#define FILE_COMPLETE_IF_OPLOCKED               0x00000100
+#define FILE_NO_EA_KNOWLEDGE                    0x00000200
+#define FILE_OPEN_REMOTE_INSTANCE               0x00000400
+#define FILE_RANDOM_ACCESS                      0x00000800
+
+#define FILE_DELETE_ON_CLOSE                    0x00001000
+#define FILE_OPEN_BY_FILE_ID                    0x00002000
+#define FILE_OPEN_FOR_BACKUP_INTENT             0x00004000
+#define FILE_NO_COMPRESSION                     0x0000800
+
+#define FILE_SUPERSEDE                  0x00000000
+#define FILE_OPEN                       0x00000001
+#define FILE_CREATE                     0x00000002
+#define FILE_OPEN_IF                    0x00000003
+#define FILE_OVERWRITE                  0x00000004
+#define FILE_OVERWRITE_IF               0x00000005
+#define FILE_MAXIMUM_DISPOSITION        0x00000005
+
+#define OBJ_INHERIT             0x00000002L
+#define OBJ_PERMANENT           0x00000010L
+#define OBJ_EXCLUSIVE           0x00000020L
+#define OBJ_CASE_INSENSITIVE    0x00000040L
+#define OBJ_OPENIF              0x00000080L
+#define OBJ_OPENLINK            0x00000100L
+#define OBJ_VALID_ATTRIBUTES    0x000001F2L
+
+typedef __success(return >= 0) LONG NTSTATUS;
+#define STATUS_SUCCESS   ((NTSTATUS)0x00000000L)
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+
+#define InitializeObjectAttributes( p, n, a, r, s ) { \
+    (p)->Length = sizeof( OBJECT_ATTRIBUTES );          \
+    (p)->RootDirectory = r;                             \
+    (p)->Attributes = a;                                \
+    (p)->ObjectName = n;                                \
+    (p)->SecurityDescriptor = s;                        \
+    (p)->SecurityQualityOfService = NULL;               \
+    }
+
+typedef enum _FILE_INFORMATION_CLASS {
+    FileDirectoryInformation = 1,
+    FileFullDirectoryInformation,                   // 2
+    FileBothDirectoryInformation,                   // 3
+    FileBasicInformation,                           // 4
+    FileStandardInformation,                        // 5
+    FileInternalInformation,                        // 6
+    FileEaInformation,                              // 7
+    FileAccessInformation,                          // 8
+    FileNameInformation,                            // 9
+    FileRenameInformation,                          // 10
+    FileLinkInformation,                            // 11
+    FileNamesInformation,                           // 12
+    FileDispositionInformation,                     // 13
+    FilePositionInformation,                        // 14
+    FileFullEaInformation,                          // 15
+    FileModeInformation,                            // 16
+    FileAlignmentInformation,                       // 17
+    FileAllInformation,                             // 18
+    FileAllocationInformation,                      // 19
+    FileEndOfFileInformation,                       // 20
+    FileAlternateNameInformation,                   // 21
+    FileStreamInformation,                          // 22
+    FilePipeInformation,                            // 23
+    FilePipeLocalInformation,                       // 24
+    FilePipeRemoteInformation,                      // 25
+    FileMailslotQueryInformation,                   // 26
+    FileMailslotSetInformation,                     // 27
+    FileCompressionInformation,                     // 28
+    FileObjectIdInformation,                        // 29
+    FileCompletionInformation,                      // 30
+    FileMoveClusterInformation,                     // 31
+    FileQuotaInformation,                           // 32
+    FileReparsePointInformation,                    // 33
+    FileNetworkOpenInformation,                     // 34
+    FileAttributeTagInformation,                    // 35
+    FileTrackingInformation,                        // 36
+    FileIdBothDirectoryInformation,                 // 37
+    FileIdFullDirectoryInformation,                 // 38
+    FileValidDataLengthInformation,                 // 39
+    FileShortNameInformation,                       // 40
+    FileIoCompletionNotificationInformation,        // 41
+    FileIoStatusBlockRangeInformation,              // 42
+    FileIoPriorityHintInformation,                  // 43
+    FileSfioReserveInformation,                     // 44
+    FileSfioVolumeInformation,                      // 45
+    FileHardLinkInformation,                        // 46
+    FileProcessIdsUsingFileInformation,             // 47
+    FileNormalizedNameInformation,                  // 48
+    FileNetworkPhysicalNameInformation,             // 49
+    FileIdGlobalTxDirectoryInformation,             // 50
+    FileIsRemoteDeviceInformation,                  // 51
+    FileUnusedInformation,                          // 52
+    FileNumaNodeInformation,                        // 53
+    FileStandardLinkInformation,                    // 54
+    FileRemoteProtocolInformation,                  // 55
+
+        //
+        //  These are special versions of these operations (defined earlier)
+        //  which can be used by kernel mode drivers only to bypass security
+        //  access checks for Rename and HardLink operations.  These operations
+        //  are only recognized by the IOManager, a file system should never
+        //  receive these.
+        //
+
+        FileRenameInformationBypassAccessCheck,         // 56
+        FileLinkInformationBypassAccessCheck,           // 57
+
+            //
+            // End of special information classes reserved for IOManager.
+            //
+
+            FileVolumeNameInformation,                      // 58
+            FileIdInformation,                              // 59
+            FileIdExtdDirectoryInformation,                 // 60
+            FileReplaceCompletionInformation,               // 61
+            FileHardLinkFullIdInformation,                  // 62
+            FileIdExtdBothDirectoryInformation,             // 63
+            FileDispositionInformationEx,                   // 64
+            FileRenameInformationEx,                        // 65
+            FileRenameInformationExBypassAccessCheck,       // 66
+            FileDesiredStorageClassInformation,             // 67
+            FileStatInformation,                            // 68
+            FileMemoryPartitionInformation,                 // 69
+            FileStatLxInformation,                          // 70
+            FileCaseSensitiveInformation,                   // 71
+            FileLinkInformationEx,                          // 72
+            FileLinkInformationExBypassAccessCheck,         // 73
+            FileStorageReserveIdInformation,                // 74
+            FileCaseSensitiveInformationForceAccessCheck,   // 75
+            FileKnownFolderInformation,   // 76
+
+            FileMaximumInformation
+} FILE_INFORMATION_CLASS, * PFILE_INFORMATION_CLASS;
+
 typedef struct _IO_STATUS_BLOCK {
     union {
         NTSTATUS Status;
@@ -16,6 +160,28 @@ VOID
     _In_ ULONG Reserved
     );
 
+
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES {
+    ULONG           Length;
+    HANDLE          RootDirectory;
+    PUNICODE_STRING ObjectName;
+    ULONG           Attributes;
+    PVOID           SecurityDescriptor;
+    PVOID           SecurityQualityOfService;
+} OBJECT_ATTRIBUTES,*POBJECT_ATTRIBUTES;
+
+typedef struct _FILE_NAME_INFORMATION {
+    ULONG FileNameLength;
+    WCHAR FileName[1];
+} FILE_NAME_INFORMATION, * PFILE_NAME_INFORMATION;
+
+
 extern "C"
 {
 
@@ -29,6 +195,39 @@ extern "C"
         _In_     ULONG            Length,
         _In_opt_ PLARGE_INTEGER   ByteOffset,
         _In_opt_ PULONG           Key
+    );
+
+
+
+    NTSYSAPI VOID RtlInitUnicodeString(
+        PUNICODE_STRING         DestinationString,
+        PCWSTR SourceString
+    );
+
+    __kernel_entry NTSTATUS NtCreateFile(
+        PHANDLE            FileHandle,
+        ACCESS_MASK        DesiredAccess,
+        POBJECT_ATTRIBUTES ObjectAttributes,
+        PIO_STATUS_BLOCK   IoStatusBlock,
+        PLARGE_INTEGER     AllocationSize,
+        ULONG              FileAttributes,
+        ULONG              ShareAccess,
+        ULONG              CreateDisposition,
+        ULONG              CreateOptions,
+        PVOID              EaBuffer,
+        ULONG              EaLength
+    );
+
+    __kernel_entry  NTSTATUS NtQueryInformationFile(
+        HANDLE                 FileHandle,
+        PIO_STATUS_BLOCK       IoStatusBlock,
+        PVOID                  FileInformation,
+        ULONG                  Length,
+        FILE_INFORMATION_CLASS FileInformationClass
+    );
+
+    __kernel_entry NTSTATUS NtClose(
+        HANDLE Handle
     );
 
 
