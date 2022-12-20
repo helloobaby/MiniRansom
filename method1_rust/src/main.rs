db8b586bcbfffed01b1267d8e806d108cb4d60cc
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-12-20 14:52:48 +0800

    rust traversee file

--- method1_rust/src/main.rs
+++ method1_rust/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use walkdir::WalkDir;
+use lazy_static::lazy_static;
+use std::{str, vec};
+
+lazy_static!{
+static ref kIncludeBackfix:Vec<&'static str> = vec![".exe",".dll",".png",".jpg",
+".bmp", ".txt", ".ini",  ".sys"];
+
+static ref kHardDriver:Vec<&'static str> = vec!["C:"];
+}
+
+pub fn encrypt_buffer() {
+
+}
+
+pub fn encrypt_file(){
+    
+}
+
+fn main() -> Result<(),walkdir::Error>{
+    
+    for HardDriver in kHardDriver.iter(){
+
+    for entry in WalkDir::new(HardDriver) {
+
+        match entry{
+            Ok(DirEntry)  => {
+
+                // 判断指定文件是否符合目标后缀
+                let BackfixInclude = false;
+                for iter in kIncludeBackfix.iter(){
+                    if(DirEntry.file_name().to_str().unwrap().ends_with(iter)){
+                        // 打印文件
+                        println!("{}", DirEntry.path().display());
+                    }
+                }
+                
+            },
+            Err(error) => {
+                
+            }
+        }
+	}
+    }
+
+Ok(())
 }
